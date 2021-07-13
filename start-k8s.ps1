9aa5d3f6c7d6f4784c647a342cd0c3bc1a13c0ab
sujeetkp <sujeetkupadhi@gmail.com>
2021-07-13 23:11:33 +0530

    Still Working

--- start-k8s.ps1
+++ start-k8s.ps1
@@ -1,10 +1,15 @@
-kubectl apply -f ./k8s/app-persistent-volume-claim.yml
-kubectl apply -f ./k8s/db-persistent-volume-claim.yml
+kubectl apply -f ./k8s/postgres-persistent-volume-claim.yml
+kubectl apply -f ./k8s/app-1-persistent-volume-claim.yml
 kubectl apply -f ./k8s/postgres-deployment.yml
 kubectl apply -f ./k8s/postgres-service.yml
 Start-Sleep 60
-kubectl apply -f ./k8s/app-deployment.yml
-kubectl apply -f ./k8s/app-service.yml
+kubectl apply -f ./k8s/app-1-deployment.yml
+kubectl apply -f ./k8s/app-1-service.yml
+kubectl apply -f ./k8s/app-1-hpa.yml
+kubectl apply -f ./k8s/app-2-deployment.yml
+kubectl apply -f ./k8s/app-2-service.yml
 Start-Sleep 20
+kubectl apply -f ./k8s/app-1-nginx-deployment.yml
+kubectl apply -f ./k8s/app-1-nginx-service.yml
 kubectl apply -f ./k8s/nginx-deployment.yml
 kubectl apply -f ./k8s/nginx-service.yml
