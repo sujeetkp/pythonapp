6c7d48b0d573fe8ae8354944a5985815c311192d
sujeetkp <sujeetkupadhi@gmail.com>
2021-07-14 20:55:20 +0530

    included nginx ingress

--- start-k8s.ps1
+++ start-k8s.ps1
@@ -11,5 +11,10 @@ kubectl apply -f ./k8s/app-2-service.yml
 Start-Sleep 20
 kubectl apply -f ./k8s/app-1-nginx-deployment.yml
 kubectl apply -f ./k8s/app-1-nginx-service.yml
-kubectl apply -f ./k8s/nginx-deployment.yml
-kubectl apply -f ./k8s/nginx-service.yml
+
+# Use Below for Nginx Deployment and Service
+#kubectl apply -f ./k8s/nginx-deployment.yml
+#kubectl apply -f ./k8s/nginx-service.yml
+
+# Use below for Nginx Ingress Controller
+kubectl apply -f ./k8s/ingress-service.yml
